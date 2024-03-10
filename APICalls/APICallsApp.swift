b77a8159dfa90d4ccb6e0ebe5b0b82202b9c2986
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-03-10 17:44:40 +0100

    Initial Commit

---
+++ APICalls/APICallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  APICallsApp.swift
+//  APICalls
+//
+//  Created by Vladyslav Dikhtiaruk on 10/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct APICallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
